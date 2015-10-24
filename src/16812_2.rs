57ee9fa9578ea996f1bdbe4d1b92cae5d4b5d34d
Bruno Tavares <btavare@thoughtworks.com>
2015-10-24 00:58:10 -0200

    ICE: Unexpected type returned from struct_tai
    Reference rust-lang/rust#16812

---
+++ src/16812_2.rs
@@ -0,0 +1,18 @@
+use std::path::Path;
+use std::path::PathBuf;
+
+pub enum Source
+{
+	File(Path), // compiler error :(
+	//File(PathBuf), // works :)
+}
+
+pub fn source_to_dbtype(src: &Source) -> u8
+{
+	match src
+	{
+		&Source::File(..) => 1,
+	}
+}
+
+fn main() {}
