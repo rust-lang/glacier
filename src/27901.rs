9a9e78c56e69776d706e8c01cf718f3abca9f538
Steve Klabnik <steve@steveklabnik.com>
2017-03-15 12:09:46 -0400

    Delete 27901.rs
    @rust-lang/rust#27901 has been fixed

--- src/27901.rs
+++
@@ -1,6 +0,0 @@
-trait Stream { type Item; }
-impl<'a> Stream for &'a str { type Item = char; }
-fn f<'s>(_: &'s str) -> (&'s str, <&'s str as Stream>::Item) { loop {}}
-fn main() {
-    f as for<'t> fn(&'t str) -> (&'t str, <&'t str as Stream>::Item);
-}
