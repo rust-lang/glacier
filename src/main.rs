0e9b582d38856f4442c95b87c5f167b16c546fd5
Alex Macleod <alex@macleod.io>
2019-10-09 16:18:09 +0100

    Make root crate into a lib

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,20 @@
+use glacier::Outcome;
 use rayon::prelude::*;
 use std::error::Error;
-use std::path::Path;
-use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
 
-mod ice;
+fn main() -> Result<(), Box<dyn Error>> {
+    let failed = glacier::test_all()?
+        .map(|res| -> Result<bool, String> {
+            let result = res?;
+            println!("{}", result);
 
-use ice::{Outcome, TestResult, ICE};
-
-static ICES_PATH: &str = "ices";
-
-fn test(path: &Path) -> Result<(), Box<dyn Error>> {
-    let failed = Arc::new(AtomicBool::new(false));
-    let result_lock = Arc::new(Mutex::new(()));
-    ice::discover(path)?
-        .par_iter()
-        .map(|ice| {
-            let result = ice.test().map_err(|e| e.to_string())?;
-            report_result(result_lock.clone(), &ice, &result);
-            if result.outcome() != Outcome::ICEd {
-                failed.store(true, Ordering::SeqCst);
-            }
-            Ok(())
+            Ok(result.outcome() != Outcome::ICEd)
         })
-        .collect::<Result<Vec<()>, String>>()?;
+        .try_reduce(|| false, |a, b| Ok(a || b))?;
 
-    if failed.load(Ordering::SeqCst) {
-        return Err("some ICEs are now fixed!".into());
-    }
-    Ok(())
-}
-
-fn report_result(lock: Arc<Mutex<()>>, ice: &ICE, res: &TestResult) {
-    // Prevent multiple results being reported at the same time
-    let _ = lock.lock();
-
-    match res.outcome() {
-        Outcome::NoError => println!("{}: fixed with no errors", ice.path().display()),
-        Outcome::Errored => println!("{}: fixed with errors", ice.path().display()),
-        Outcome::ICEd => println!("{}: ICEd", ice.path().display()),
-    }
-    if res.outcome() != Outcome::ICEd {
-        println!("=== stdout ===");
-        println!("{}", res.stdout());
-        println!("=== stderr ===");
-        println!("{}", res.stderr());
-        println!("==============");
-    }
-}
-
-fn main() {
-    if let Err(err) = test(&Path::new(ICES_PATH)) {
-        eprintln!("error: {}", err);
-        std::process::exit(1);
+    if failed {
+        Err("some ICEs are now fixed!".into())
+    } else {
+        Ok(())
     }
 }
