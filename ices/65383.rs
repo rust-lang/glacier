d6c5367fa8b7d7ec513013596cb1821a53a4e6cf
Yuki Okushi <huyuumi.dev@gmail.com>
2019-10-14 11:41:38 +0900

    Minimize ICE example

--- ices/65383.rs
+++ ices/65383.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let c = |p| {
-        'main: for h in p..(p + 10) {
-            let j = |k| {
-                break 'main;
-            };
-            j(p);
-        }
-    };
-    c(1);
+    'lab: loop {
+        || {
+            break 'lab;
+        };
+    }
 }
