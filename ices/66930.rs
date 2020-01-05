d584d552bbb920e13581d1bb87870cfaf868826f
Yuki Okushi <huyuumi.dev@gmail.com>
2020-01-05 23:12:18 +0900

    code review

--- ices/66930.rs
+++ ices/66930.rs
@@ -1,10 +1,7 @@
-#!/bin/bash
+#![crate_type = "lib"]
 
-rustc --crate-type=lib - << EOF
 static UTF8_CHAR_WIDTH: [u8; 0] = [];
 
 pub fn utf8_char_width(b: u8) -> usize {
     UTF8_CHAR_WIDTH[b as usize] as usize
 }
-
-EOF
