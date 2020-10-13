64bad420b09b15ad969c4f7f895b5b4f2ed7008c
Olivia Crain <olivia@olivia.dev>
2020-10-12 18:01:54 -0700

    Add ICE 77735 (#503)
    * Add ICE 77735
    
    * formatting fix

---
+++ ices/77735.rs
@@ -0,0 +1,10 @@
+#![feature(platform_intrinsics)]
+#![feature(staged_api)]
+struct f32x3();
+extern "platform-intrinsic" {
+    #[rustc_const_stable(feature = "foo", since = "1.3.37")]
+    fn simd_extract<T, U>(x: T, idx: u32) -> U;
+}
+const Y0: i8 = unsafe { simd_extract(f32x3, 0) };
+
+fn main() {}
