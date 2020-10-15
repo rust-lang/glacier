0ac895fd8477e42a467c57d5fa362d78018bb916
Olivia Crain <olivia@olivia.dev>
2020-10-15 15:13:04 -0500

    Add ICE 77987

---
+++ ices/77987.rs
@@ -0,0 +1,18 @@
+#![feature(type_alias_impl_trait)]
+
+trait Foo<T> {}
+impl<T, U> Foo<T> for U {}
+
+type Scope = impl Foo<()>;
+
+#[allow(unused)]
+fn infer_scope() -> Scope {
+    ()
+}
+
+#[allow(unused)]
+fn ice() -> impl Foo<Scope> {
+    loop {}
+}
+
+fn main() {}
