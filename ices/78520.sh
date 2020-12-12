784d9c04e6d6a2e6b0afe3516c8b467c1cd05371
Padraic Fanning <fanninpm@miamioh.edu>
2020-12-11 21:59:23 -0500

    Add 10 ICEs

---
+++ ices/78520.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rustc -Zdump-mir=all - <<EOF
+// build-pass
+
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+
+struct Bug<const S: &'static str>;
+
+fn main() {
+    let b: Bug::<{
+        unsafe {
+            // FIXME(const_generics): Decide on how to deal with invalid values as const params.
+            std::mem::transmute::<&[u8], &str>(&[0xC0, 0xC1, 0xF5])
+        }
+    }>;
+}
+EOF
