13cdf7cd8ff60fa18305e4ebcda26ca48bba834d
Padraic Fanning <fanninpm@miamioh.edu>
2020-12-17 20:08:46 -0500

    Reduce 80125

--- ices/80125.rs
+++ ices/80125.rs
@@ -1,16 +1,8 @@
 type ExternCallback = extern "C" fn(*const u8, u32, str);
 
-pub struct Client {
-    // ...
-    conn_tx_bind: ExternCallback,
-    // ...
-}
+pub struct Struct(ExternCallback);
 
 #[no_mangle]
-pub extern fn register_something(bind: ExternCallback) -> Client {
-    Client{
-        conn_tx_bind: bind
-    }
+pub extern "C" fn register_something(bind: ExternCallback) -> Struct {
+    Struct(bind)
 }
-
-fn main() {}
