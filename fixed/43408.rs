a196f81bc3bc56aa57e3d1ebb49450d71f6e2dc6
Yuki Okushi <huyuumi.dev@gmail.com>
2020-12-29 08:09:38 +0900

    Tweak code

--- fixed/43408.rs
+++ fixed/43408.rs
@@ -2,6 +2,9 @@
 pub const fn sof<T:?Sized>() -> usize {
     10
 }
-fn to_byte_array<T>() -> [u8; sof::<T>()] {
-     panic!()
+
+pub fn to_byte_array<T>() -> [u8; sof::<T>()] {
+    panic!()
 }
+
+fn main() {}
