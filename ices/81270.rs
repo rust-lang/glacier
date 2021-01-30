edc5c22197e5758b42e6c23411fa1d3ec654a796
Padraic Fanning <fanninpm@miamioh.edu>
2021-01-29 20:15:21 -0500

    Kill rustfmt

--- ices/81270.rs
+++ ices/81270.rs
@@ -1,12 +1,9 @@
 #![feature(allocator_api)]
-use std::alloc::{AllocError, Allocator, Global, Layout};
-use std::marker::PhantomData;
+use std::alloc::{Layout, Allocator, Global, AllocError};
 use std::ptr::NonNull;
+use std::marker::PhantomData;
 
-struct S<A> {
-    a: PhantomData<A>,
-    b: [u8; 1],
-}
+struct S<A>{ a: PhantomData<A>, b: [u8; 1] }
 unsafe impl<A> Allocator for S<A> {
     fn allocate(&self, _: Layout) -> Result<NonNull<[u8]>, AllocError> {
         todo!();
@@ -17,11 +14,5 @@ unsafe impl<A> Allocator for S<A> {
 }
 
 fn main() {
-    let x: Box<u8, S<u8>> = Box::new_in(
-        0,
-        S {
-            a: PhantomData,
-            b: [0; 1],
-        },
-    );
+    let x: Box<u8, S<u8>> = Box::new_in(0, S { a: PhantomData, b: [0; 1] });
 }
