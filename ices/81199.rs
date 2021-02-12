4a45148f28787b4e1fe8e342904482a8f6364c3f
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-12 16:10:53 -0500

    Minimize 81199

--- ices/81199.rs
+++ ices/81199.rs
@@ -1,18 +1,9 @@
-#[repr(C)]
 union PtrRepr<T: ?Sized> {
     const_ptr: *const T,
     mut_ptr: *mut T,
-    components: PtrComponents<T>,
+    components: <T as Pointee>::Metadata
 }
-
-#[repr(C)]
-struct PtrComponents<T: Pointee + ?Sized> {
-    data_address: *const (),
-    metadata: <T as Pointee>::Metadata,
-}
-
-
 
 pub trait Pointee {
-   type Metadata;
+    type Metadata;
 }
