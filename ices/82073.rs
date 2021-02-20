742f024c882f9dcb75422c4cd0c40d17b1a9d5e9
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-20 18:12:51 -0500

    Remove 82073
    Issue closed.

--- ices/82073.rs
+++
@@ -1,8 +0,0 @@
-#![feature(let_chains)]
-#[async_std::main]
-async fn main() -> Result<()> {
-    if let Some(_) = 2 && let Some(sixteen) = 16 {
-        isize::from_str_radix("A", sixteen)?;
-    }
-    Ok(())
-}
