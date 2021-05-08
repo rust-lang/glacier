da3dd0a921bd030d41742c01f9318c2e18c37c8b
johannesd3 <johannesd@outlook.com>
2021-05-08 11:48:18 +0200

    Add 85031
    Issue: rust-lang/rust#85031

---
+++ ices/85031-2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rustc --edition 2018 -C incremental=foo --crate-type lib - <<'EOF'
+#![allow(incomplete_features)]
+#![feature(const_generics, const_evaluatable_checked)]
+
+pub struct Ref<'a>(&'a i32);
+
+impl Ref<'_> {
+    pub fn foo<const A: usize>()
+    where
+        ([(); A - 0], ()): Sized,
+    {
+        Self::bar::<A>()
+    }
+
+    fn bar<const A: usize>()
+    where
+	([(); A - 0], ()): Sized,
+    {
+    }
+}
+
+EOF
+
