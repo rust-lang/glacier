766410b8e7e8dfd274ea79d4857bfb2f4eb90a53
Alex Macleod <alex@macleod.io>
2021-05-12 13:34:26 +0100

    Use minimized repro for 53092
    Closes #744

--- ices/53092.rs
+++ ices/53092.rs
@@ -1,33 +1,16 @@
-#![feature(const_fn)]
-#![feature(const_fn_union)]
 #![feature(impl_trait_in_bindings)]
 #![feature(min_type_alias_impl_trait)]
 #![feature(type_alias_impl_trait)]
-#![feature(untagged_unions)]
-
-const fn transmute<T, U>(from: T) -> U {
-    use std::mem::ManuallyDrop;
-
-    union Transform<FROM, INTO> {
-        from: ManuallyDrop<FROM>,
-        into: ManuallyDrop<INTO>,
-    }
-
-    let transformer = Transform {
-        from: ManuallyDrop::new(from),
-    };
-
-    unsafe { ManuallyDrop::into_inner(transformer.into) }
-}
+#![allow(dead_code)]
 
 type Bug<T, U> = impl Fn(T) -> U + Copy;
 
-const CONST_BUG: Bug<u8, ()> = transmute(|_: u8| ());
+const CONST_BUG: Bug<u8, ()> = unsafe { std::mem::transmute(|_: u8| ()) };
 
 fn make_bug<T, U: From<T>>() -> Bug<T, U> {
     |x| x.into()
 }
 
 fn main() {
-    let x = CONST_BUG(0);
+    CONST_BUG(0);
 }
