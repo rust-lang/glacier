70e50e146afbb0bafaca52e300f097b53a5a09a8
Yuki Okushi <yuki.okushi@huawei.com>
2021-06-08 09:15:51 +0900

    Triage closed but tested ICEs (part 2)

--- ices/13368.rs
+++
@@ -1,7 +0,0 @@
-#![feature(llvm_asm)]
-
-fn main() {
-    let arr: [u8; 16];
-    unsafe { llvm_asm!("" : "=m"(arr)); }
-    println!("{:?}", arr);
-}
--- ices/13520.rs
+++
@@ -1,9 +0,0 @@
-#![feature(llvm_asm)]
-
-fn main() {
-    let x = ();
-    unsafe {
-        let p: *const () = &x;
-        llvm_asm!("" :: "r"(*p));
-    }
-}
