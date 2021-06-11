19d7748929bf4197c3612a18cc5f906c9e96da71
Padraic Fanning <fanninpm@miamioh.edu>
2021-06-11 18:57:41 -0400

    Add 86086
    Issue: rust-lang/rust#86086

---
+++ ices/86086.rs
@@ -0,0 +1,2 @@
+trait Tr : Sized { async extern r###"��áÖ@"### fn union < > ( # [ ']' '[' 'default' 'async' '&raw' ] & '?ဪൈ self , ... , ... , ... , ... , ... , ... , ... , ... , ... , ) -> && '_ mut ( ) ;
+}
