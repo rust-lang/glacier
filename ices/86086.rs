5bb8c22ebef48e562dcd9aed4e2766cecc833854
Yuki Okushi <yuki.okushi@huawei.com>
2021-07-06 09:53:19 +0900

    Triage some ICEs
    - issue-85350 was moved to `./ices` but actually should've NOT.
    - issue-86086 was a duplicate of another issue, it can be removed.

--- ices/86086.rs
+++
@@ -1,2 +0,0 @@
-trait Tr : Sized { async extern r###"��áÖ@"### fn union < > ( # [ ']' '[' 'default' 'async' '&raw' ] & '?ဪൈ self , ... , ... , ... , ... , ... , ... , ... , ... , ... , ) -> && '_ mut ( ) ;
-}
