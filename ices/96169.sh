dca99ead8de66afb81aead1fbf77177319e01ac8
Matthias Krüger <matthias.krueger@famsik.de>
2022-04-23 20:25:10 +0200

    add 11 ices

---
+++ ices/96169.sh
@@ -0,0 +1,22 @@
+rustc -Zmir-opt-level=4 --emit=mir - 2>&1 << EOF
+
+// run-rustfix
+#![allow(unused)]
+fn a() -> usize { 0 }
+//~^ ERROR return types are denoted using `->`
+
+fn bar(_: u32) {}
+
+fn baz() -> *const dyn Fn(u32) { unimplemented!() }
+
+fn foo() {
+    match () {
+        _ if baz() == &bar as &dyn Fn(u32) => (),
+        () => (),
+    }
+}
+
+fn main() {
+}
+
+EOF
