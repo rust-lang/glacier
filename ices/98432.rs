41f9750ed738b4f831ecaee9e0d3694aeb3afb54
Yuki Okushi <jtitor@2k36.org>
2022-06-24 14:29:15 +0900

    Update ices/98432.rs

--- ices/98432.rs
+++ ices/98432.rs
@@ -2,6 +2,11 @@ struct Struct<T>(T);
 
 impl<T> Struct<T> {
     const CONST: fn() = || {
-        struct _Obligation where T:;
+        struct _Obligation
+        where
+            T:;
     };
 }
+
+fn main() {}
+
