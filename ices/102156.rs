5c98aaa21282fcfbe5bb7a3ee2d5f5c363d18a8b
Matthias Krüger <matthias.krueger@famsik.de>
2022-09-23 18:17:06 +0200

    add 12/13 ices
    https://github.com/rust-lang/rust/issues/101964
    https://github.com/rust-lang/rust/issues/102047
    https://github.com/rust-lang/rust/issues/102089
    https://github.com/rust-lang/rust/issues/102105
    https://github.com/rust-lang/rust/issues/102114
    https://github.com/rust-lang/rust/issues/102117
    https://github.com/rust-lang/rust/issues/102124
    https://github.com/rust-lang/rust/issues/102140
    https://github.com/rust-lang/rust/issues/102154
    https://github.com/rust-lang/rust/issues/102156
    https://github.com/rust-lang/rust/issues/102172
    https://github.com/rust-lang/rust/issues/102173

---
+++ ices/102156.rs
@@ -0,0 +1,12 @@
+#![feature(allocator_api)]
+
+#![feature(const_trait_impl)]
+use core::convert::{From, TryFrom};
+use std::pin::Pin;
+use std::alloc::Allocator;
+impl<T: ?Sized, A: Allocator> const From<Box<T, A>> for Pin<Box<T, A>>
+where
+    A: 'static,
+{}
+
+pub fn main() {}
